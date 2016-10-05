4884c06935d17ed6ec43eaaea119cf04a80d1ea0
Alexander Tong <tongsalex@gmail.com>
2016-10-05 19:19:37 -0400

    add first questions and functionality

--- game.js
+++ game.js
@@ -1,14 +1,48 @@
 $(document).ready(function() { //LOAD DOM
 console.log('i am ready');
 
-function unblur () {
-  $('.container').css('filter','none')
+//MUSIC FILES
+var ledzep = new Audio("LedZeppelin.mp3");
+var kansas = new Audio("Kansas.mp3");
+var eagles = new Audio("TheEagles.mp3");
+var songs = [{
+  song: ledzep,
+  bandname: 'LED ZEPPELIN'
+  }, {
+    song: kansas,
+    bandname: 'KANSAS'
+  }];
+
+//VARIABLES
+var score = 0;
+var strikes = 0;
+var songNumber = 0;
+
+//FUNCTIONS
+function checkValue (event) {
+  var input = $('.answer')[0].value;
+  input = input.toUpperCase(); //makes all input uppercase so that we can check it easier even if the user decides to use caps or no caps.
+  if (input == songs[songNumber].bandname) {
+  score ++;
+  }
+  else {
+    strikes ++;
+  }
+  $('.answer')[0].value = ''; //resets the input text field for the next question
+  console.log(score);
+  console.log(strikes);
+  songNumber++; //Song changes to next one in array after each submit.
 }
 
-function reblur () {
-  $('.container').css('filter','blur(10px)')
+
+function playMusic() {
+  soundTrack = songs[songNumber].song; //plays audio when startpage is loaded
+  soundTrack.play();
+  $('.play').unbind('click'); //FIX THIS. USER HAS TO BE ABLE TO CLICK AGAIN
 }
 
-$('.start').hover(unblur, reblur)
 
+//EVENT LISTENERS
+$('.play').click(playMusic); //Music only plays when button is clicked.
+$('.submit').click(checkValue);
 });
--- startpage.js
+++ startpage.js
@@ -12,38 +12,11 @@ function reblur () {
 $('.start').hover(unblur, reblur)
 
 function playMusic() {
-      //creates a new rockStar from prototype in the function piece below
-    soundTrack = new Audio("heartbreaker.mp3"); //plays audio when game is loaded
+    soundTrack = new Audio("heartbreaker.mp3"); //plays audio when startpage is loaded
     soundTrack.play();
 }
+
 playMusic();
-
-  // var vid = document.getElementById("bgvid");
-  // var pauseButton = document.querySelector("#polina button");
-  //
-  // function vidFade() {
-  //   vid.classList.add("stopfade");
-  // }
-  //
-  // vid.addEventListener('ended', function()
-  // {
-  // // only functional if "loop" is removed
-  // vid.pause();
-  // // to capture IE10
-  // vidFade();
-  // });
-
-  // pauseButton.addEventListener("click", function() {
-  //   vid.classList.toggle("stopfade");
-  //   if (vid.paused) {
-  //     vid.play();
-  //     pauseButton.innerHTML = "Pause";
-  //   } else {
-  //     vid.pause();
-  //     pauseButton.innerHTML = "Paused";
-  //   }
-  // })
-
 
 
 });
